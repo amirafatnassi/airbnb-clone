1b5747da71046534da68a2d6bd6f380e4e1dd858
amirafatnassi <amirafatnassi88@gmail.com>
2022-09-02 20:12:37 +0100

    update search ts

--- src/app/features/header/header.component.ts
+++ src/app/features/header/header.component.ts
@@ -1,4 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ThisReceiver } from '@angular/compiler';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { IonContent } from '@ionic/angular';
@@ -8,13 +16,21 @@ import { IonContent } from '@ionic/angular';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, AfterViewInit {
+  //get search-bar html element reference
+  @ViewChild('serachbar',{static:true, read: ElementRef}) public searchbar!:ElementRef<HTMLIonRowElement>;
+  
   @Input() public ionContent!: IonContent;
   @Input() public places!: any[];
-  public segmentVisible!: string;
+  public segmentVisible!: string|undefined;
   public form!: FormGroup;
 
-  constructor(private readonly _router: Router) {}
+  constructor(
+    private readonly _router: Router,
+    private readonly _ref: ElementRef<HTMLElement>
+  ) {
+    this.segmentVisible = 'places';
+  }
 
   ngOnInit(): void {
     //generate airbnb form
@@ -36,6 +52,32 @@ export class HeaderComponent implements OnInit {
     });
   }
 
+  ngAfterViewInit(): void {
+    if (!this.ionContent?.scrollEvents) {
+      this._ref.nativeElement.classList.add('scrolled');
+      this._ref.nativeElement.classList.add('static');
+      //  this._ref.nativeElement.classList.add('active');
+      return;
+    }
+    this.ionContent.scrollEvents = true;
+    this.ionContent.ionScroll.subscribe(($event) => {
+      this.scrolling($event);
+    });
+  }
+
+  scrolling($event: any) {
+    const { detail: { scrollTop = 0 } = {} } = $event;
+    if (!this._ref?.nativeElement) {
+      return;
+    }
+    if (scrollTop > 5) {
+      this._ref.nativeElement.classList.add('scrolled');
+      this.segmentVisible = undefined;
+      if (this.searchbar)
+        this.searchbar.nativeElement.classList.remove('active');
+    } else {
+    }
+  }
   async activate(segmentName: string) {
     this.segmentVisible = segmentName;
   }
